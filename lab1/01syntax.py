0e125ac73e8cb49c6aea7fe6b80abf4bb7c89ba4
Salima <salima.askarova17@gmail.com>
2024-01-24 00:41:47 +0600

    added lab1

---
+++ lab1/01syntax.py
@@ -0,0 +1,7 @@
+#ex.1
+print("Hello World")
+#answer: print
+
+#ex.2
+if 5 > 2:
+    print("YES")
---
+++ lab1/04datatypes.py
@@ -0,0 +1,34 @@
+#ex.1
+x = 5
+print(type(x))
+#answer: int
+
+#ex.2
+x = "Hello World"
+print(type(x))
+#asnwer: string
+
+#ex.3
+x = 20.5
+print(type(x))
+#answer: float
+
+#ex.4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#answer: list
+
+#ex.5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#answer: tuple
+
+#ex.6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#answer: dict
+
+#ex.7
+x = True
+print(type(x))
+#answer: bool
