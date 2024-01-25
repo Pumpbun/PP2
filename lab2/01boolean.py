f117146409b4be834c0700861d6cbee5dc5e099e
Salima <salima.askarova17@gmail.com>
2024-01-25 17:12:07 +0600

    added lab2

---
+++ lab2/01boolean.py
@@ -0,0 +1,19 @@
+#ex.1
+print(10 > 9)
+#answer: True
+
+#ex.2
+print(10 == 9)
+#answer: False
+
+#ex.3
+print(10 < 9)
+#answer: False
+
+#ex.4
+print(bool("abc"))
+#answer: True
+
+#ex.5
+print(bool(0))
+#answer: False
